22a28fee08284cf522f17e3f9125b4f3f626c2f8
Andree Honoree <iradonoree@gmail.com>
2023-09-05 01:13:22 +0300

    New files are added in components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 //import logo from './logo.svg';
 import './App.css';
 import React,{ Component} from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <Home/>
-        <About></About>
-      </div>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='about' element={<About />}/>
+        </Routes>
+      </>
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
 import React, {Component }from 'react'
+import '../index.css';
+
+
 
 class Home extends Component{
     render(){
-        return <h1>Welcome to Fast Tracking Application</h1>
+       return(
+        <div className='home'>
+          <h1>Welcome to Fast Tracking Application</h1>
+          <p>This website helps people to orders from their homes and trach their orders without calling the company</p>
+        </div>
+       )
     }
 }
 
